f835380451ac8977b53c23e57506526930474c47
ayevexy <ayevexy@proton.me>
2026-01-25 10:33:53 -0300

    feat: add for_each macro

---
+++ src/util/for_each.h
@@ -0,0 +1,26 @@
+#ifndef FOR_EACH_H
+#define FOR_EACH_H
+
+#include "list/array_list.h"
+
+#include "util/collection.h"
+#include "util/iterator.h"
+
+/**
+ * @brief A utility macro to iterate sequentially through the elements of a collection.
+ *
+ * @param variable the variable which will hold the current element
+ * @param collection the collection to iterate through
+ */
+#define for_each(variable, collection)                                                          \
+    for (Iterator* _iter_ = collection_iterator(_Generic(collection,                            \
+            ArrayList*: array_list_to_collection,                                               \
+            Collection: collection                                                              \
+        )(collection));                                                                         \
+        _iter_;                                                                                 \
+        iterator_delete(&_iter_)                                                                \
+    )                                                                                           \
+        for (void* _value_; iterator_has_next(_iter_) && (_value_ = iterator_next(_iter_));)    \
+            for (variable = _value_, * _flag_ = (void*) 0xDEADBEEF; _flag_; _flag_ = nullptr)
+
+#endif
---
+++ test/util/for_each_test.c
@@ -0,0 +1,47 @@
+#include "util/for_each.h"
+
+#include "../list/array_list_test.h"
+#include "list/array_list.h"
+
+#include "unity.h"
+
+static ArrayList* array_list;
+
+void setUp() {
+    array_list = array_list_new(DEFAULT_ARRAY_LIST_OPTIONS());
+}
+
+void tearDown() {
+    array_list_delete(&array_list);
+}
+
+void test_iterate_using_for_each_construct() {
+    // given
+    int values[] = { 1, 2, 3, 4, 5 }, new_values[5], count = 0;
+    POPULATE_ARRAY_LIST(array_list, values);
+    // when
+    for_each (int* element, array_list) {
+        new_values[count++] = *element;
+    }
+    // then
+    TEST_ASSERT_ARRAY_EQUALS(new_values, (int**) &values);
+    TEST_ASSERT_EQUAL(5, count);
+}
+
+void test_iterate_using_for_each_construct_on_empty_collection() {
+    // given
+    int count = 0;
+    // when
+    for_each (int* element, array_list) {
+        count++;
+    }
+    // then
+    TEST_ASSERT_EQUAL(0, count);
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_iterate_using_for_each_construct);
+    RUN_TEST(test_iterate_using_for_each_construct_on_empty_collection);
+    return UNITY_END();
+}

7920939fd1f7f7f36f658c5ee154a473ea39a2c1
ayevexy <ayevexy@proton.me>
2026-03-11 12:28:25 -0300

    refactor(constraints): prefix internal macros with underscore

--- src/util/constraints.h
+++ src/util/constraints.h
@@ -11,28 +11,28 @@
  *
  * @param ... one or more pointers to check.
  */
-#define require_non_null(...) dispatch_require_non_null(__VA_ARGS__,    \
-    require_non_null_6,                                                 \
-    require_non_null_5,                                                 \
-    require_non_null_4,                                                 \
-    require_non_null_3,                                                 \
-    require_non_null_2,                                                 \
-    require_non_null_1                                                  \
+#define require_non_null(...) _dispatch_require_non_null(__VA_ARGS__,    \
+    _require_non_null_6,                                                 \
+    _require_non_null_5,                                                 \
+    _require_non_null_4,                                                 \
+    _require_non_null_3,                                                 \
+    _require_non_null_2,                                                 \
+    _require_non_null_1                                                  \
 )(__VA_ARGS__)
 
-#define dispatch_require_non_null(_1, _2, _3, _4, _5, _6, NAME, ...) NAME
+#define _dispatch_require_non_null(_1, _2, _3, _4, _5, _6, NAME, ...) NAME
 
-#define require_non_null_6(pointer, ...) (require_non_null_1(pointer) || require_non_null_5(__VA_ARGS__))
+#define _require_non_null_6(pointer, ...) (_require_non_null_1(pointer) || _require_non_null_5(__VA_ARGS__))
 
-#define require_non_null_5(pointer, ...) (require_non_null_1(pointer) || require_non_null_4(__VA_ARGS__))
+#define _require_non_null_5(pointer, ...) (_require_non_null_1(pointer) || _require_non_null_4(__VA_ARGS__))
 
-#define require_non_null_4(pointer, ...) (require_non_null_1(pointer) || require_non_null_3(__VA_ARGS__))
+#define _require_non_null_4(pointer, ...) (_require_non_null_1(pointer) || _require_non_null_3(__VA_ARGS__))
 
-#define require_non_null_3(pointer, ...) (require_non_null_1(pointer) || require_non_null_2(__VA_ARGS__))
+#define _require_non_null_3(pointer, ...) (_require_non_null_1(pointer) || _require_non_null_2(__VA_ARGS__))
 
-#define require_non_null_2(pointer, ...) (require_non_null_1(pointer) || require_non_null_1(__VA_ARGS__))
+#define _require_non_null_2(pointer, ...) (_require_non_null_1(pointer) || _require_non_null_1(__VA_ARGS__))
 
-#define require_non_null_1(pointer) \
+#define _require_non_null_1(pointer) \
     ((pointer) == nullptr ? set_error(NULL_POINTER_ERROR, "'"#pointer"'"" argument must not be null"), true : false)
 
 #endif
